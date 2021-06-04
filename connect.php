9ad4f145eb70139b3682c00e79e2b3fe1ed02600
KhangLe <lephuckhang1005@gmail.com>
2021-06-05 01:39:58 +0700

    change database address

--- connect.php
+++ connect.php
@@ -1,5 +1,12 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ecommerce");
+// $conn = new mysqli("localhost", "root", "", "ecommerce");
+
+// if ($conn->connect_errno) {
+//     echo "Failed to connect to MySQL: " . $conn->connect_error;
+//     exit();
+// }
+
+$conn = new mysqli("remotemysql.com", "dYOBa5KYD8", "G4nJxXSlDJ", "dYOBa5KYD8");
 
 if ($conn->connect_errno) {
     echo "Failed to connect to MySQL: " . $conn->connect_error;
--- db.php
+++ db.php
@@ -1,14 +1,23 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "ecommerce";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $db = "ecommerce";
 
-// Create connection
+// $con = mysqli_connect($servername, $username, $password, $db);
+
+// if (!$con) {
+//     die("Connection failed: " . mysqli_connect_error());
+// }
+
+$servername = "remotemysql.com";
+$username = "dYOBa5KYD8";
+$password = "G4nJxXSlDJ";
+$db = "dYOBa5KYD8";
+
 $con = mysqli_connect($servername, $username, $password, $db);
 
-// Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
