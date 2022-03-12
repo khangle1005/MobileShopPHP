10a47495deb41258e09dd7729a8db8ac6e446d5b
khangle1005 <75433346+khangle1005@users.noreply.github.com>
2022-03-12 15:31:14 +0700

    Update DB admin

--- admin/connect.php
+++ admin/connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ecommerce");
+$conn = new mysqli("us-cdbr-east-05.cleardb.net:3306:3306", "bc53acb34c57c9", "82e4a0ce", "heroku_5f2e6a0f4cd89b0");
 
 if ($conn->connect_errno) {
     echo "Failed to connect to MySQL: " . $conn->connect_error;
