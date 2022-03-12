c6be6513b8402cb9bc9206270f82fe20a8893277
khangle1005 <75433346+khangle1005@users.noreply.github.com>
2022-03-12 15:27:19 +0700

    update DB address

--- db.php
+++ db.php
@@ -11,10 +11,10 @@
 //     die("Connection failed: " . mysqli_connect_error());
 // }
 
-$servername = "remotemysql.com:3306";
-$username = "dYOBa5KYD8";
-$password = "G4nJxXSlDJ";
-$db = "dYOBa5KYD8";
+$servername = "us-cdbr-east-05.cleardb.net:3306:3306";
+$username = "bc53acb34c57c9";
+$password = "82e4a0ce";
+$db = "heroku_5f2e6a0f4cd89b0";
 
 $con = mysqli_connect($servername, $username, $password, $db);
 
