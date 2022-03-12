bdd0f2a9a74f4f9311ca2f2722efee37c2993aef
khangle1005 <75433346+khangle1005@users.noreply.github.com>
2022-03-12 15:27:50 +0700

    Update DB address

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@
 //     exit();
 // }
 
-$conn = new mysqli("remotemysql.com:3306", "dYOBa5KYD8", "G4nJxXSlDJ", "dYOBa5KYD8");
+$conn = new mysqli("us-cdbr-east-05.cleardb.net:3306:3306", "bc53acb34c57c9", "82e4a0ce", "heroku_5f2e6a0f4cd89b0");
 
 if ($conn->connect_errno) {
     echo "Failed to connect to MySQL: " . $conn->connect_error;
